5a3582766650e5fdcb25a21236fc9e182e465520
Vadym <vadymkravtcsov@gmail.com>
2025-08-26 23:24:56 +0300

    add Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -2,7 +2,7 @@
 # n people, numbered from 1 to n, stand in a circle. 
 # They start counting, and every k-th person is eliminated from the circle. 
 # The count then continues with the next person. 
-# Write a program that determines the number of the person who remains last in the circle.
+# Write a program that determines the number of the person who remains last in the circle.     
 
 n, k = int(input()), int(input())
 m = list(range(1, n+1 ))
