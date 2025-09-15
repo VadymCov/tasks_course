45542fe846030c74e940c06ead29e02c5d480711
Vadym <vadymkravtcsov@gmail.com>
2025-09-15 19:56:47 +0300

    add setMethod Task_7.py 'four number problems'

--- setMethods/Task_7.py
+++ setMethods/Task_7.py
@@ -1,58 +1,58 @@
-# # # 🔢 **Number of Matches**
-# # # The program receives two lines of text containing numbers. Write a program that determines 
-# # # the number of numbers that are present in both the first line and the second line.
-# # # 
-# # # **Input Format**
-# # # The program receives two lines of text containing numbers separated by spaces.
-# # # 
-# # # **Output Format** 
-# # # The program should output the number of numbers contained simultaneously 
-# # # in both the first line and the second line.
-# # # Test _____________________________________________________
-
-# # s1, s2 = set(input().split()), set(input().split())
-# # s1.intersection_update(s2)
-# # print(len(s1))
-# # #___________________________________________________________
-
-
-
-# # # 🔢 **Common Numbers**
-# # # The program receives two lines of text containing numbers. Write a program that outputs 
-# # # all numbers in ascending order that are present in both the first line and the second line.
-# # # 
-# # # **Input Format**
-# # # The program receives two lines of text containing integers separated by spaces.
-# # # 
-# # # **Output Format** 
-# # # The program should output on one line, separated by spaces, the numbers that appear 
-# # # in both lines.
-
-# # # Test____________________________________________________
-
-# # s1, s2 = [set(map(int, input().split())) for _ in ['s1', 's2']]
-# # result = sorted(s1.intersection(s2))
-# # print(*result)
-# # #_________________________________________________________
-
-
-
-# # 🔢 **Numbers of the First Line**
-# # The program receives two lines of text containing numbers. Write a program that outputs 
-# # all numbers in ascending order that are present in the first line but absent in the second.
+# # 🔢 **Number of Matches**
+# # The program receives two lines of text containing numbers. Write a program that determines 
+# # the number of numbers that are present in both the first line and the second line.
 # # 
 # # **Input Format**
 # # The program receives two lines of text containing numbers separated by spaces.
 # # 
 # # **Output Format** 
-# # The program should output the set of numbers that appear only in the first line.
+# # The program should output the number of numbers contained simultaneously 
+# # in both the first line and the second line.
+# # Test _____________________________________________________
+
+s1, s2 = set(input().split()), set(input().split())
+s1.intersection_update(s2)
+print(len(s1))
+# #___________________________________________________________
 
 
-# # # Test____________________________________________________
 
-# s1, s2 = [set(map(int, input().split())) for _ in range(2)]
-# print(*sorted(s1.difference(s2)))
-# #_____________________________________________________________
+# # 🔢 **Common Numbers**
+# # The program receives two lines of text containing numbers. Write a program that outputs 
+# # all numbers in ascending order that are present in both the first line and the second line.
+# # 
+# # **Input Format**
+# # The program receives two lines of text containing integers separated by spaces.
+# # 
+# # **Output Format** 
+# # The program should output on one line, separated by spaces, the numbers that appear 
+# # in both lines.
+
+# # Test____________________________________________________
+
+s1, s2 = [set(map(int, input().split())) for _ in ['s1', 's2']]
+result = sorted(s1.intersection(s2))
+print(*result)
+# #_________________________________________________________
+
+
+
+# 🔢 **Numbers of the First Line**
+# The program receives two lines of text containing numbers. Write a program that outputs 
+# all numbers in ascending order that are present in the first line but absent in the second.
+# 
+# **Input Format**
+# The program receives two lines of text containing numbers separated by spaces.
+# 
+# **Output Format** 
+# The program should output the set of numbers that appear only in the first line.
+
+
+# # Test____________________________________________________
+
+s1, s2 = [set(map(int, input().split())) for _ in range(2)]
+print(*sorted(s1.difference(s2)))
+#_____________________________________________________________
 
 
 
@@ -60,11 +60,11 @@
 # The program receives a natural number n, and then n different natural numbers, 
 # each on a separate line. Write a program that outputs all common digits in 
 # ascending order among all entered numbers.
-# 
+
 # **Input Format**
 # The program receives a natural number n (n≥1), and then n different natural numbers, 
 # each on a separate line.
-# 
+
 # **Output Format** 
 # The program should output the digits according to the task condition. 
 # If there are no common digits, then nothing should be output.
