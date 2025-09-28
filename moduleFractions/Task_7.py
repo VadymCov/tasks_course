d6f696cb9727d5bf7a0e0c3365c271c07db912f7
Vadym <vadymkravtcsov@gmail.com>
2025-09-28 14:18:38 +0300

    add moduleFractions Task_7.py 'Ordered Fractions'

---
+++ moduleFractions/Task_7.py
@@ -0,0 +1,29 @@
+# 📊 Ordered Fractions
+#
+# Input: A natural number n. Write a program that outputs in ascending order all
+# irreducible fractions between 0 and 1, whose denominator does not exceed n.
+#
+# Input Format
+# A natural number n (n > 1).
+#
+# Output Format
+# Output the desired fractions, each on a separate line.
+#
+# Note. You may need the gcd() function to find the greatest common divisor (GCD)
+# of two numbers. The function is implemented in the math module.
+
+# Test _____________________________________________
+
+from fractions import Fraction as F
+from math import gcd
+
+n = int(input())
+fraction_list = []
+for i in range(1, n):
+    for j in range(n, 1, -1):
+        if i > j:
+            break
+        if gcd(i, j) == 1:
+            fraction_list.append(F(i, j))
+            
+print(*sorted(fraction_list), sep="\n")
