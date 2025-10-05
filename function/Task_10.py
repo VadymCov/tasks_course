89512467690f562606e1aa5763302f4bfa10476c
Vadym <vadymkravtcsov@gmail.com>
2025-10-05 16:06:35 +0300

    add function Task_10.py ''

---
+++ function/Task_10.py
@@ -0,0 +1,34 @@
+# 🧠 Write a program that uses the built-in functions filter(), map(), sorted(),
+# and reduce() to output an alphabetically sorted list of primary
+# cities with a population greater than 10,000,000, in the following format.
+
+from functools import reduce
+
+data = [['Tokyo', 35676000, 'primary'],
+        ['New York', 19354922, 'nan'],
+        ['Mexico City', 19028000, 'primary'],
+        ['Mumbai', 18978000, 'admin'],
+        ['Sao Paulo', 18845000, 'admin'],
+        ['Delhi', 15926000, 'admin'],
+        ['Shanghai', 14987000, 'admin'],
+        ['Kolkata', 14787000, 'admin'],
+        ['Los Angeles', 12815475, 'nan'],
+        ['Dhaka', 12797394, 'primary'],
+        ['Buenos Aires', 12795000, 'primary'],
+        ['Karachi', 12130000, 'admin'],
+        ['Cairo', 11893000, 'primary'],
+        ['Rio de Janeiro', 11748000, 'admin'],
+        ['Osaka', 11294000, 'admin'],
+        ['Beijing', 11106000, 'primary'],
+        ['Manila', 11100000, 'primary'],
+        ['Moscow', 10452000, 'primary'],
+        ['Istanbul', 10061000, 'admin'],
+        ['Paris', 9904000, 'primary']]
+
+filter_result = filter(lambda d: d[1] > 10_000_000 and d[2] == "primary", data)
+
+map_result = map(lambda x : x[0], filter_result)
+
+sort_result = sorted(map_result)
+
+print("Cities:", reduce(lambda x, y: x + ", " + y, sort_result)
